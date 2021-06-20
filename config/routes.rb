6c908920f816c1b0bbb4c18b58598aa48498d39b
nakagawa-ren <hh.hakoware@gmail.com>
2021-06-20 13:34:21 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to:'homes#top'
+  devise_for :users
+    resources :books, only: [:create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
