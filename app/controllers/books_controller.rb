7ea3c898e82e2327d559bff3d522e4cd0aaff9d4
nakagawa-ren <hh.hakoware@gmail.com>
2021-06-21 06:06:52 +0000

    users index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,9 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to:'homes#top'
   devise_for :users
     resources :books, only: [:create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users,only: [:show,:index,:edit,:update]
 end
