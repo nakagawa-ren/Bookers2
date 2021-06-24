e89dda69098b50b5253ef7f43d49de3e4120994c
nakagawa-ren <hh.hakoware@gmail.com>
2021-06-24 10:11:21 +0000

    Final

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @user = @book.user
+    if @user.id != current_user.id
+      redirect_to books_path
+    end
   end
 
     def update
